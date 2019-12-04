44c68ad793053225a2ae87f5d94cd194bd8f37c1
An Chengrui <35605703+purewaltzan@users.noreply.github.com>
2019-12-04 10:48:10 +0800

    Add files via upload

--- tsnetplugin.R
+++ tsnetplugin.R
@@ -1,13 +1,16 @@
 if(!require('tsne')){
   install.packages('tsne')
+  library(tsne)
 }
 if(!require('rstudioapi')){
   install.packages('rstudioapi')
+  library(rstudioapi)
 }
 if(!require('matlabr')){
   install.packages('matlabr')
+  library(matlabr)
 }
-libpath<-(dirname(rstudioapi::getSourceEditorContext()$path))
+libpath<-"/media/sf_D_DRIVE/H/tsneTransformation"
 
 dim.reduction <- setClass(
   Class = "DimReduc",
@@ -35,7 +38,8 @@ tsneTransform<-function(object,
                         seed.use = 42,
                         approx = TRUE,
                         outputfile = NULL,
-                        deleteTemp = TRUE,
+                        deleteTemp = FALSE,
+                        slot = 'data',
                         ...){
   if (!is.null(x = seed.use)) {
     set.seed(seed = seed.use)
@@ -44,49 +48,61 @@ tsneTransform<-function(object,
   if (is.numeric(x = cells)) {
     cells <- colnames(x = object)[cells]
   }
-  if(platform=='R'){
-    warning("Trying tSNE transformation in R. If there are more than 2000 cells, it will take very long time. Try to use platform='MATLAB'")
-    tsner<-tsne(t(as.matrix(GetAssayData(object))),
+  raw_data<-as.matrix(GetAssayData(object,slot))
+  if(length(object@assays$RNA@var.features>10)){
+    raw_data<-raw_data[object@assays$RNA@var.features,]
+  }
+  if(platform=='outputfiles'){
+    if(is.null(outputfile)){
+      outputfile<-paste(libpath,'/tsnetoutput.csv',sep='')
+      tsner<-read.csv(outputfile,header = FALSE, row.names = NULL)
+    }
+  }else{
+    if(platform=='R'){
+      warning("Trying tSNE transformation in R. If there are more than 2000 cells, it will take very long time. Try to use platform='MATLAB'")
+      tsner<-tsne(t(raw_data),
                 perplexity = perp,
                 initial_dims = dim,
                 k = dim,
-    )
-  }else{
-    if(platform=='MATLAB'){
-      if(have_matlab()){
-        write.csv(t(as.matrix(GetAssayData(object))),file=paste(libpath,'/data.csv',sep=''))
-        write.csv(c(perp,dim),file=paste(libpath,'/para.csv',sep=''))
-        run_matlab_script(fname=paste(libpath,'/tsnet.m',sep=''), verbose = TRUE, desktop = FALSE,
+                )
+      }else{
+        if(platform=='MATLAB'){
+          get_matlab()
+          if(have_matlab()){
+            write.csv(t(raw_data),file=paste(libpath,'/data.csv',sep=''))
+            write.csv(c(perp,dim),file=paste(libpath,'/para.csv',sep=''))
+            run_matlab_script(fname=paste(libpath,'/tsnet.m',sep=''), verbose = TRUE, desktop = FALSE,
                           splash = FALSE, display = FALSE, wait = TRUE,
                           single_thread = FALSE, ...)
-        outputfile<-paste(libpath,'/tsnetoutput.csv',sep='')
-        tsner<-read.csv(outputfile,header = FALSE, row.names = NULL)
-        deleteTemp = TRUE
-      }else{
-        warn('There is no MATLAB detected in this environment, attempting to use output files')
-        if(is.null(outputfile)){
-          outputfile<-paste(libpath,'/tsnetoutput.csv',sep='')
-        }
-        tsner<-try(outputfile,header = FALSE, row.names = NULL)
-        if(!is.array(tsner)){
-          stop("There is no t-SNE transformation output files. Please locate it or generate a new one or use platform = 'R'")
-        }else{
-          deleteTemp = FALSE
-        }
+            outputfile<-paste(libpath,'/tsnetoutput.csv',sep='')
+            tsner<-read.csv(outputfile,header = FALSE, row.names = NULL)
+            #deleteTemp = TRUE
+            }else{
+              warning('There is no MATLAB detected in this environment, attempting to use output files')
+              if(is.null(outputfile)){
+                outputfile<-paste(libpath,'/tsnetoutput.csv',sep='')
+                }
+              tsner<-try(outputfile,header = FALSE, row.names = NULL)
+              if(!is.array(tsner)){
+                stop("There is no t-SNE transformation output files. Please locate it or generate a new one or use platform = 'R'")
+                }else{
+                  deleteTemp = FALSE
+                }
+              }
+          if(deleteTemp){
+            file.remove(paste(libpath,'/data.csv',sep=''))
+            file.remove(paste(libpath,'/para.csv',sep=''))
+            file.remove(paste(libpath,'/tsnetoutput.csv',sep=''))
+            }
+          }else{
+            stop('t-SNE transform should be run in R or MATLAB')
+          }
       }
-      if(deleteTemp){
-        file.remove(paste(libpath,'/data.csv',sep=''))
-        file.remove(paste(libpath,'/para.csv',sep=''))
-        file.remove(paste(libpath,'/tsnetoutput.csv',sep=''))
-      }
-    }else{
-      stop('t-SNE transform should be run in R or MATLAB')
     }
-  }
   cna<-vector()
   for(i in 1:ncol(tsner)){
     cna<-c(cna,paste('tsnet_',i,sep=''))
-  }
+    }
   colnames(tsner)<-cna
   rownames(tsner)<-cells
   ttf.obj <- new(Class = "DimReduc",cell.embeddings = as.matrix(tsner))
@@ -94,3 +110,4 @@ tsneTransform<-function(object,
   DefaultAssay(object@reductions[[reduction]])<-"RNA"
   return(object)
 }
+
